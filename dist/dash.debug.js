d58dc2b09a16200da5ba38acf9b5febb154d2581
Dahoon Kim <salqueng@gmail.com>
2015-08-05 16:48:42 +0900

    Maintain playback rate during stalled state
    Conflicts:
    	dist/dash.all.js
    	dist/dash.debug.js
    	dist/dash.min.js

--- dist/dash.debug.js
+++ dist/dash.debug.js
@@ -10364,12 +10364,13 @@ MediaPlayer.models.URIQueryAndFragmentModel.prototype = {
 
 MediaPlayer.models.VideoModel = function() {
     "use strict";
-    var element, stalledStreams = [], isStalled = function() {
+    var element, stalledStreams = [], playbackRateBeforeStalled = 1, isStalled = function() {
         return stalledStreams.length > 0;
     }, addStalledStream = function(type) {
         if (type === null || element.seeking) {
             return;
         }
+        playbackRateBeforeStalled = this.getPlaybackRate();
         this.setPlaybackRate(0);
         if (stalledStreams[type] === true) {
             return;
@@ -10386,7 +10387,7 @@ MediaPlayer.models.VideoModel = function() {
             stalledStreams.splice(index, 1);
         }
         if (isStalled() === false) {
-            this.setPlaybackRate(1);
+            this.setPlaybackRate(playbackRateBeforeStalled);
         }
     }, stallStream = function(type, isStalled) {
         if (isStalled) {
